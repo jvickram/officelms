2941ea342b8507efef938bd233610585f75262a1
Manojm Kumar <manojm.kumar@oneadvanced.com>
2019-03-13 14:51:45 +0530

    Registration Screen UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/HomeComponent';
+import Registration from './components/auth/RegistrationComponent';
+import Login from './components/auth/LoginComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        {/* <Home /> */}
+        {/* <Login/> */}
+        <Registration />
       </div>
     );
   }
--- src/components/auth/RegistrationComponent.js
+++ src/components/auth/RegistrationComponent.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Col, } from 'reactstrap';
 
 class Registration extends Component {
   render() {
     return (
-      <div>
-        <h1>Employee Registration</h1>
+      <div  className = "abc">
+        <h1 style={{textAlign:"center"}}> Employee Registration</h1>
+        <Form className = "abc">
+          <FormGroup row className = "abc">
+            <Label md={2}>EmployeeID</Label>
+            <Col md={4}>
+              <Input type = "text" placeholder = "EmpID" />
+            </Col>
+          </FormGroup>
+          <FormGroup row style = {{justifyContent:'center'}}>
+            <Label md={2}>First Name</Label>
+            <Col md={4}>
+              <Input type = "text" placeholder = "First Name" />
+            </Col>
+          </FormGroup>
+          <FormGroup row style = {{justifyContent:'center'}}>
+            <Label md={2}>Last Name</Label>
+            <Col md={4}>
+              <Input type = "text" placeholder = "Last Name" />
+            </ Col>
+          </FormGroup>
+          <FormGroup row style = {{justifyContent:'center'}}>
+          <Label md={2}>Email</Label>
+            <Col md={4}>
+              <Input type = "email" placeholder = "Email" />
+            </ Col>
+          </FormGroup>
+          <FormGroup row style = {{justifyContent:'center'}}>
+            <Label md={2}>Password</Label>
+            <Col md={4}> 
+              <Input type = "password" placeholder = "Password" />
+            </ Col>
+          </FormGroup>
+          <FormGroup row style = {{justifyContent:'center'}}>
+          <Label md={2}>Confirm Password</Label>
+            <Col md={4}>
+              <Input type = "password" placeholder = "Confirm Password" />
+            </ Col>
+          </FormGroup>
+            <FormGroup row style = {{justifyContent:'center'}}>
+            <Col md={{size:4,offset:4}}>
+             <Button>Sign Up</Button>
+             <Button style = {{marginLeft: '30px'}}>Reset</Button>
+             </Col>
+            </FormGroup>
+        </Form>
       </div>
     )
   }
