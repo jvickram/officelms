641451dc83f4998c245c1a635a1b3d472d829407
Manojm Kumar <manojm.kumar@oneadvanced.com>
2019-03-13 16:32:24 +0530

    Code Refactor

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         {/* <Home /> */}
-        {/* <Login/> */}
-        <Registration />
+        <Login/>
+        {/* <Registration /> */}
       </div>
     );
   }
--- src/components/auth/RegistrationComponent.js
+++ src/components/auth/RegistrationComponent.js
@@ -4,46 +4,46 @@ import { Button, Form, FormGroup, Label, Input, Col, } from 'reactstrap';
 class Registration extends Component {
   render() {
     return (
-      <div  className = "abc">
+      <div>
         <h1 style={{textAlign:"center"}}> Employee Registration</h1>
-        <Form className = "abc">
-          <FormGroup row className = "abc">
+        <Form>
+          <FormGroup row className = "justification">
             <Label md={2}>EmployeeID</Label>
             <Col md={4}>
               <Input type = "text" placeholder = "EmpID" />
             </Col>
           </FormGroup>
-          <FormGroup row style = {{justifyContent:'center'}}>
+          <FormGroup row className = "justification">
             <Label md={2}>First Name</Label>
             <Col md={4}>
               <Input type = "text" placeholder = "First Name" />
             </Col>
           </FormGroup>
-          <FormGroup row style = {{justifyContent:'center'}}>
+          <FormGroup row className = "justification" style = {{justifyContent:'center'}}>
             <Label md={2}>Last Name</Label>
             <Col md={4}>
               <Input type = "text" placeholder = "Last Name" />
             </ Col>
           </FormGroup>
-          <FormGroup row style = {{justifyContent:'center'}}>
+          <FormGroup row className = "justification" style = {{justifyContent:'center'}}>
           <Label md={2}>Email</Label>
             <Col md={4}>
               <Input type = "email" placeholder = "Email" />
             </ Col>
           </FormGroup>
-          <FormGroup row style = {{justifyContent:'center'}}>
+          <FormGroup row className = "justification">
             <Label md={2}>Password</Label>
             <Col md={4}> 
               <Input type = "password" placeholder = "Password" />
             </ Col>
           </FormGroup>
-          <FormGroup row style = {{justifyContent:'center'}}>
+          <FormGroup row className = "justification">
           <Label md={2}>Confirm Password</Label>
             <Col md={4}>
               <Input type = "password" placeholder = "Confirm Password" />
             </ Col>
           </FormGroup>
-            <FormGroup row style = {{justifyContent:'center'}}>
+            <FormGroup row className = "justification">
             <Col md={{size:4,offset:4}}>
              <Button>Sign Up</Button>
              <Button style = {{marginLeft: '30px'}}>Reset</Button>
